9e143bf044aed0d260f2f2eb33946157dffd3786
Crowdbotics <team@crowdbotics.com>
2023-05-18 09:57:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wandering-wood-40816",
+  url: "https://wandering-wood-40816.botics.co",
+  api: "https://wandering-wood-40816.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
